11549b4a274e6c257e39fad8862c585a224ffde3
pavel asadchy <asadchy.pavel@gmail.com>
2023-01-18 23:59:16 +0100

    init: ws

---
+++ remote-control-server/src/enums.ts
@@ -0,0 +1,8 @@
+export enum WS_EVENT {
+  START = 'start',
+  CONNECTION = 'connection',
+  MESSAGE = 'message',
+  RESPONSE = 'response',
+  CLOSE = 'close',
+  ERROR = 'error'
+}
--- remote-control-server/src/main.ts
+++ remote-control-server/src/main.ts
@@ -1,1 +1,17 @@
-console.log('Hello')
+import WebSocket, { WebSocketServer } from 'ws';
+import 'dotenv/config';
+
+import { WS_EVENT } from './enums';
+
+const port = Number(process.env.PORT);
+
+const wss = new WebSocketServer({ port });
+
+wss.on(WS_EVENT.CONNECTION, (ws: WebSocket) => {
+  console.log('connected')
+  ws.on(WS_EVENT.MESSAGE, (data) => {
+    console.log('received: %s', data);
+  });
+
+  ws.send('something');
+});
